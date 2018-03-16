de22e5cec7e20d557287e54bd09119380075e274
Dylan Carty <dylancarty8@gmail.com>
2018-03-15 21:53:17 -0700

    Update game.py
    Added Game logic to it's own file for ease of access and readability.

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
-import sys
 import pygame as pg
 from settings import Music_Mixer, loadCustomFont, States
 
-class LevelOne(States):
+#Subclass of States
+#Can make subclasses of the subclass
+#Used to load game assets and do runtime stuff.
+class Game(States):
     def __init__(self):
         States.__init__(self)
-        self.next = 'mainmenu'
+        self.next = 'levelone'
     def cleanup(self):
         print('cleaning up Game state stuff')
     def startup(self):
@@ -13,6 +15,27 @@ class LevelOne(States):
     def get_event(self, event):
         if event.type == pg.KEYDOWN:
             print('Game State keydown')
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            pg.mixer.music.stop()
+            self.done = True
+    def update(self, screen, dt):
+        self.draw(screen)
+    def draw(self, screen):
+        screen.fill((255, 0, 0))
+
+#Level One of the game.
+class LevelOne(States):
+    def __init__(self):
+        States.__init__(self)
+        self.next = 'mainmenu'
+
+    def cleanup(self):
+        print('cleaning up Game Level One state stuff')
+    def startup(self):
+        print('starting Game Level One state stuff')
+    def get_event(self, event):
+        if event.type == pg.KEYDOWN:
+            print('Game Level One State keydown')
         elif event.type == pg.MOUSEBUTTONDOWN:
             pg.mixer.music.play()
             self.done = True
